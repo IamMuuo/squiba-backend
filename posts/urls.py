d7852cbd0ce2c9cec38156effe97f7aa649d6670
Erick <hearteric57@gmail.com>
2024-03-01 23:04:00 +0300

    feat: got application to retrieve all user's posts for the profile page

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,16 @@
+"""Posts url file.
+
+Contains all the urls related to posts
+"""
+
 from django.urls import path
-from .views import PostList
+from .views import PostList, UserPostListing
 
 urlpatterns = [
-    path("listings/", PostList.as_view(), name="story-list"),
+    path("listings/", PostList.as_view(), name="post-listing"),
+    path(
+        "find/<int:user_id>",
+        UserPostListing.as_view(),
+        name="find-post-listing",
+    ),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
-from rest_framework import generics
+"""Post views.
 
+Contains all views related to the Post service
+"""
+
+from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
+    """PostList.
+
+    Handles functionality for post creation
+    """
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserPostListing(generics.ListAPIView):
+    """UserPostListing.
+
+    Retrieves all posts pertaining a certain user
+    """
+
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        """Return a actual user posts."""
+        user_id = self.kwargs["user_id"]
+        return Post.objects.filter(user_id=user_id)
