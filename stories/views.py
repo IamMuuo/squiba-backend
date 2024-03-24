e9d998c0c00db344f435aefee9111d4ae1a0039b
Erick <hearteric57@gmail.com>
2024-03-24 23:20:00 +0300

    chore: fixed stories views to conform to pep8

--- stories/views.py
+++ stories/views.py
@@ -1,4 +1,6 @@
-from datetime import datetime, timezone
+"""StoriesViews."""
+
+from datetime import datetime
 from rest_framework import generics
 from .models import Story
 from .serializers import StorySerializer
@@ -7,15 +9,25 @@ from .serializers import StorySerializer
 
 
 class StoryList(generics.ListCreateAPIView):
+    """StoryList.
+
+    Create a story
+    """
+
     queryset = Story.objects.all()
     serializer_class = StorySerializer
 
     def get_queryset(self):
-        # Filter stories to only include those that have not yet expired
+        """Filter stories to only include those that have not yet expired."""
         return Story.objects.filter(date_of_expiry__gt=datetime.now())
 
 
 # Retrieve, update or delete a story instance
 class StoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    """StoryDetail.
+
+    UD (Update Delete) operations on the story model
+    """
+
     queryset = Story.objects.all()
     serializer_class = StorySerializer
